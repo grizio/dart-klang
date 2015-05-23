36259643c13528d45ba831cc170812aa49566e9b
Gaëtan Rizio <gaetan@rizio.fr>
2015-05-23 14:06:08 +0200

    Add functions in string utilities.

--- lib/utilities/string.dart
+++ lib/utilities/string.dart
@@ -12,3 +12,122 @@ String nvl(String val1, String val2) {
     return val1;
   }
 }
+
+/// Checks if given string is null or empty.
+///
+///     isEmpty(null) // true
+///     isEmpty('')   // true
+///     isEmpty(' ')  // false
+///     isEmpty('a')  // false
+bool isEmpty(String str) => str == null || str.isEmpty;
+
+/// Checks if given string is not null nor empty.
+///
+///     isNotEmpty(null) // false
+///     isNotEmpty('')   // false
+///     isNotEmpty(' ')  // true
+///     isNotEmpty('a')  // true
+bool isNotEmpty(String str) => !isEmpty(str);
+
+/// Checks if given string is null or blank.
+///
+///     isBlank(null) // true
+///     isBlank('')   // true
+///     isBlank(' ')  // true
+///     isBlank('a')  // false
+bool isBlank(String str) => str == null || str.trim().isEmpty;
+
+/// Checks if given string is not null nor blank.
+///
+///     isNotBlank(null) // false
+///     isNotBlank('')   // false
+///     isNotBlank(' ')  // false
+///     isNotBlank('a')  // true
+bool isNotBlank(String str) => !isBlank(str);
+
+/// Checks if given strings are equals.
+///
+///     equals(null, null) // true
+///     equals(null, 'a')  // false
+///     equals('a', null)  // false
+///     equals('a', 'a')   // true
+///     equals('a', 'A')   // false
+bool equals(String str1, String str2) => str1 == null && str2 == null || str1 != null && str2 != null && str1 == str2;
+
+/// Checks if given strings are not equals.
+///
+///     notEquals(null, null) // false
+///     notEquals(null, 'a')  // true
+///     notEquals('a', null)  // true
+///     notEquals('a', 'a')   // false
+///     notEquals('a', 'A')   // true
+bool notEquals(String str1, String str2) => !equals(str1, str2);
+
+/// Checks if given strings are equals (case insensitive).
+///
+///     equalsIgnoreCase(null, null) // true
+///     equalsIgnoreCase(null, 'a')  // false
+///     equalsIgnoreCase('a', null)  // false
+///     equalsIgnoreCase('a', 'a')   // true
+///     equalsIgnoreCase('a', 'A')   // true
+bool equalsIgnoreCase(String str1, String str2) => str1 == null && str2 == null || str1 != null && str2 != null && str1.toLowerCase() == str2.toLowerCase();
+
+/// Checks if given strings are not equals (case insensitive).
+///
+///     notEqualsIgnoreCase(null, null) // false
+///     notEqualsIgnoreCase(null, 'a')  // true
+///     notEqualsIgnoreCase('a', null)  // true
+///     notEqualsIgnoreCase('a', 'a')   // false
+///     notEqualsIgnoreCase('a', 'A')   // false
+bool notEqualsIgnoreCase(String str1, String str2) => !equalsIgnoreCase(str1, str2);
+
+/// Checks if a given [str] is a secured string for password considering some rules.
+///
+/// * If [needLetter], [str] must have at least one letter (lowercase or uppercase).
+/// * If [needNumber], [str] must have at least one number.
+/// * If [needUppercaseLetter], [str) must have at least one uppercase letter.
+/// * If [needLowercaseLetter], [str] must have at least one lowercase letter.
+/// * If [needSymbol], [str] must have at least one character which is not a letter or a number.
+/// * [str] length must be equals or greater than given [minLength].
+bool isSecuredPassword(String str,
+                       {bool needLetter: true,
+                        bool needNumber: true,
+                        bool needUppercaseLetter: true,
+                        bool needLowercaseLetter: true,
+                        bool needSymbol: true,
+                        int minLength: 8}) {
+  if (str == null || str.length < minLength) {
+    return false;
+  } else if (needLetter && !str.contains(new RegExp(r'[a-zA-Z]'))) {
+    return false;
+  } else if (needNumber && !str.contains(new RegExp(r'[0-9]'))) {
+    return false;
+  } else if (needUppercaseLetter && !str.contains(new RegExp(r'[A-Z]'))) {
+    return false;
+  } else if (needLowercaseLetter && !str.contains(new RegExp(r'[a-z]'))) {
+    return false;
+  } else if (needSymbol && !str.contains(new RegExp(r'[^a-zA-Z0-9]'))) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+/// Checks if a given [str] is not a secured string for password considering some rules.
+/// See [isSecuredPassword] for rules.
+bool isNotSecuredPassword(String str,
+                          {bool needLetter: true,
+                           bool needNumber: true,
+                           bool needUppercaseLetter: true,
+                           needLowercaseLetter: true,
+                           bool needSymbol: true,
+                           int minLength: 8}) {
+  return !isSecuredPassword(
+      str,
+      needLetter: needLetter,
+      needNumber: needNumber,
+      needUppercaseLetter: needUppercaseLetter,
+      needLowercaseLetter: needLowercaseLetter,
+      needSymbol: needSymbol,
+      minLength: minLength);
+}
--- test/utilities/test_string.dart
+++ test/utilities/test_string.dart
@@ -1,21 +1,273 @@
 library klang.test.utilities.string;
 
 import 'package:unittest/unittest.dart';
-import 'package:klang/utilities/string.dart';
+import 'package:klang/utilities/string.dart' as stringUtilities;
 
 void tst_utilities_string() {
   group('utilities.string', () {
     group('nvl', () {
       test('nvl("a", null) = "a"', () {
-        expect(nvl('a', null), 'a');
+        expect(stringUtilities.nvl('a', null), 'a');
       });
 
       test('nvl(null, "b") = "b"', () {
-        expect(nvl(null, 'b'), 'b');
+        expect(stringUtilities.nvl(null, 'b'), 'b');
       });
 
       test('nvl("a", "b") = "a"', () {
-        expect(nvl('a', 'b'), 'a');
+        expect(stringUtilities.nvl('a', 'b'), 'a');
+      });
+    });
+
+    group('isEmpty', () {
+      test('isEmpty(null) = true', () {
+        expect(stringUtilities.isEmpty(null), true);
+      });
+
+      test('isEmpty("") = true', () {
+        expect(stringUtilities.isEmpty(''), true);
+      });
+
+      test('isEmpty(" ") = false', () {
+        expect(stringUtilities.isEmpty(' '), false);
+      });
+
+      test('isEmpty("a") = false', () {
+        expect(stringUtilities.isEmpty('a'), false);
+      });
+    });
+
+    group('isNotEmpty', () {
+      test('isNotEmpty(null) = false', () {
+        expect(stringUtilities.isNotEmpty(null), false);
+      });
+
+      test('isNotEmpty("") = false', () {
+        expect(stringUtilities.isNotEmpty(''), false);
+      });
+
+      test('isNotEmpty(" ") = true', () {
+        expect(stringUtilities.isNotEmpty(' '), true);
+      });
+
+      test('isNotEmpty("a") = true', () {
+        expect(stringUtilities.isNotEmpty('a'), true);
+      });
+    });
+
+    group('isBlank', () {
+      test('isBlank(null) = true', () {
+        expect(stringUtilities.isBlank(null), true);
+      });
+
+      test('isBlank("") = true', () {
+        expect(stringUtilities.isBlank(''), true);
+      });
+
+      test('isBlank(" ") = true', () {
+        expect(stringUtilities.isBlank(' '), true);
+      });
+
+      test('isBlank("a") = false', () {
+        expect(stringUtilities.isBlank('a'), false);
+      });
+    });
+
+    group('isNotBlank', () {
+      test('isNotBlank(null) = false', () {
+        expect(stringUtilities.isNotBlank(null), false);
+      });
+
+      test('isNotBlank("") = false', () {
+        expect(stringUtilities.isNotBlank(''), false);
+      });
+
+      test('isNotBlank(" ") = false', () {
+        expect(stringUtilities.isNotBlank(' '), false);
+      });
+
+      test('isNotBlank("a") = true', () {
+        expect(stringUtilities.isNotBlank('a'), true);
+      });
+    });
+
+    group('equals', () {
+      test('equals(null, null) = true', () {
+        expect(stringUtilities.equals(null, null), true);
+      });
+
+      test('equals(null, "a") = false', () {
+        expect(stringUtilities.equals(null, 'a'), false);
+      });
+
+      test('equals("a", null) = false', () {
+        expect(stringUtilities.equals('a', null), false);
+      });
+
+      test('equals("a", "a") = true', () {
+        expect(stringUtilities.equals('a', 'a'), true);
+      });
+
+      test('equals("a", "A") = false', () {
+        expect(stringUtilities.equals('a', 'A'), false);
+      });
+    });
+
+    group('notEquals', () {
+      test('notEquals(null, null) = false', () {
+        expect(stringUtilities.notEquals(null, null), false);
+      });
+
+      test('notEquals(null, "a") = true', () {
+        expect(stringUtilities.notEquals(null, 'a'), true);
+      });
+
+      test('notEquals("a", null) = true', () {
+        expect(stringUtilities.notEquals('a', null), true);
+      });
+
+      test('notEquals("a", "a") = false', () {
+        expect(stringUtilities.notEquals('a', 'a'), false);
+      });
+
+      test('notEquals("a", "A") = true', () {
+        expect(stringUtilities.notEquals('a', 'A'), true);
+      });
+    });
+
+    group('equalsIgnoreCase', () {
+      test('equalsIgnoreCase(null, null) = true', () {
+        expect(stringUtilities.equalsIgnoreCase(null, null), true);
+      });
+
+      test('equalsIgnoreCase(null, "a") = false', () {
+        expect(stringUtilities.equalsIgnoreCase(null, 'a'), false);
+      });
+
+      test('equalsIgnoreCase("a", null) = false', () {
+        expect(stringUtilities.equalsIgnoreCase('a', null), false);
+      });
+
+      test('equalsIgnoreCase("a", "a") = true', () {
+        expect(stringUtilities.equalsIgnoreCase('a', 'a'), true);
+      });
+
+      test('equalsIgnoreCase("a", "A") = false', () {
+        expect(stringUtilities.equalsIgnoreCase('a', 'A'), true);
+      });
+    });
+
+    group('notEqualsIgnoreCase', () {
+      test('notEqualsIgnoreCase(null, null) = false', () {
+        expect(stringUtilities.notEqualsIgnoreCase(null, null), false);
+      });
+
+      test('notEqualsIgnoreCase(null, "a") = true', () {
+        expect(stringUtilities.notEqualsIgnoreCase(null, 'a'), true);
+      });
+
+      test('notEqualsIgnoreCase("a", null) = true', () {
+        expect(stringUtilities.notEqualsIgnoreCase('a', null), true);
+      });
+
+      test('notEqualsIgnoreCase("a", "a") = false', () {
+        expect(stringUtilities.notEqualsIgnoreCase('a', 'a'), false);
+      });
+
+      test('notEqualsIgnoreCase("a", "A") = false', () {
+        expect(stringUtilities.notEqualsIgnoreCase('a', 'A'), false);
+      });
+    });
+
+    group('isSecuredPassword', () {
+      test('isSecuredPassword("Abcdef012345-") = true', () {
+        expect(stringUtilities.isSecuredPassword('Abcdef012345-'), true);
+      });
+
+      test('isSecuredPassword("Abcdef@012345") = true', () {
+        expect(stringUtilities.isSecuredPassword('Abcdef@012345'), true);
+      });
+
+      test('isSecuredPassword("A@eiu90u+9u") = true', () {
+        expect(stringUtilities.isSecuredPassword('A@eiu90u+9u'), true);
+      });
+
+      test('isSecuredPassword("Abcdef012345") = false', () {
+        expect(stringUtilities.isSecuredPassword('Abcdef012345'), false);
+      });
+
+      test('isSecuredPassword("Abcdefghijk-") = false', () {
+        expect(stringUtilities.isSecuredPassword('Abcdefghijk-'), false);
+      });
+
+      test('isSecuredPassword("abcdef012345-") = false', () {
+        expect(stringUtilities.isSecuredPassword('abcdef012345-'), false);
+      });
+
+      test('isSecuredPassword("Ab0-") = false', () {
+        expect(stringUtilities.isSecuredPassword('Ab0-'), false);
+      });
+
+      test('isSecuredPassword("abcdef012345", !needUppercaseLetter, !needLowercaseLetter, !needSymbol) = true', () {
+        expect(stringUtilities.isSecuredPassword('abcdef012345', needUppercaseLetter: false, needLowercaseLetter: false, needSymbol: false), true);
+      });
+
+      test('isSecuredPassword("ABCDEF012345", !needUppercaseLetter, !needLowercaseLetter, !needSymbol) = true', () {
+        expect(stringUtilities.isSecuredPassword('ABCDEF012345', needUppercaseLetter: false, needLowercaseLetter: false, needSymbol: false), true);
+      });
+
+      test('isSecuredPassword("abcdefghij", !needUppercaseLetter, !needLowercaseLetter, !needSymbol) = false', () {
+        expect(stringUtilities.isSecuredPassword('abcdefghij', needUppercaseLetter: false, needLowercaseLetter: false, needSymbol: false), false);
+      });
+
+      test('isSecuredPassword("012345678", !needUppercaseLetter, !needLowercaseLetter, !needSymbol) = false', () {
+        expect(stringUtilities.isSecuredPassword('012345678', needUppercaseLetter: false, needLowercaseLetter: false, needSymbol: false), false);
+      });
+    });
+
+    group('isNotSecuredPassword', () {
+      test('isNotSecuredPassword("Abcdef012345-") = false', () {
+        expect(stringUtilities.isNotSecuredPassword('Abcdef012345-'), false);
+      });
+
+      test('isNotSecuredPassword("Abcdef@012345") = false', () {
+        expect(stringUtilities.isNotSecuredPassword('Abcdef@012345'), false);
+      });
+
+      test('isNotSecuredPassword("A@eiu90u+9u") = false', () {
+        expect(stringUtilities.isNotSecuredPassword('A@eiu90u+9u'), false);
+      });
+
+      test('isNotSecuredPassword("Abcdef012345") = true', () {
+        expect(stringUtilities.isNotSecuredPassword('Abcdef012345'), true);
+      });
+
+      test('isNotSecuredPassword("Abcdefghijk-") = true', () {
+        expect(stringUtilities.isNotSecuredPassword('Abcdefghijk-'), true);
+      });
+
+      test('isNotSecuredPassword("abcdef012345-") = true', () {
+        expect(stringUtilities.isNotSecuredPassword('abcdef012345-'), true);
+      });
+
+      test('isNotSecuredPassword("Ab0-") = true', () {
+        expect(stringUtilities.isNotSecuredPassword('Ab0-'), true);
+      });
+
+      test('isNotSecuredPassword("abcdef012345", !needUppercaseLetter, !needLowercaseLetter, !needSymbol) = false', () {
+        expect(stringUtilities.isNotSecuredPassword('abcdef012345', needUppercaseLetter: false, needLowercaseLetter: false, needSymbol: false), false);
+      });
+
+      test('isNotSecuredPassword("ABCDEF012345", !needUppercaseLetter, !needLowercaseLetter, !needSymbol) = false', () {
+        expect(stringUtilities.isNotSecuredPassword('ABCDEF012345', needUppercaseLetter: false, needLowercaseLetter: false, needSymbol: false), false);
+      });
+
+      test('isNotSecuredPassword("abcdefghij", !needUppercaseLetter, !needLowercaseLetter, !needSymbol) = true', () {
+        expect(stringUtilities.isNotSecuredPassword('abcdefghij', needUppercaseLetter: false, needLowercaseLetter: false, needSymbol: false), true);
+      });
+
+      test('isNotSecuredPassword("012345678", !needUppercaseLetter, !needLowercaseLetter, !needSymbol) = true', () {
+        expect(stringUtilities.isNotSecuredPassword('012345678', needUppercaseLetter: false, needLowercaseLetter: false, needSymbol: false), true);
       });
     });
   });
